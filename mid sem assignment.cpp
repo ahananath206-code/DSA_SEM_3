1cb12db7eb58b747c9ea6c0e4364d252db189958
ahananath206-code <ahananath206@gmail.com>
2026-09-22 20:40:43 +0530

    Add files via upload

---
+++ mid sem assignment.cpp
@@ -0,0 +1,42 @@
+Assignment mid Sem,
+#include <stdio.h>
+int partition (int arr[], int low, int high) {
+    int pivot = arr [high];
+    int i = (low - 1);
+    for (int j = low; j < high; j++) {
+        if (arr [j] <= pivot) {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    int temp = arr[i + 1];
+    arr [i + 1] = arr [high];
+    arr [high] = temp;
+    return (i + 1);
+}
+void quicksort (int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition (arr, low, high);
+        quicksort (arr, low, pi - 1);
+        quicksort (arr, pi + 1, high);
+    }
+}
+void bubblesort (int arr[], int size) {
+    for (int step = 0; step < size; step++) {
+        for (int i = 0; i < size - step - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+    }
+    printf ("\n Internal array:\n");
+    for (int i = 0; i < size; i++) {
+        printf ("%d", arr[i]);
+    }
+    quicksort (arr, 0, size - 1);
+    break;
+}
