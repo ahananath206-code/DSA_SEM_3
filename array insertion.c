8398c0abb8812e103b0415e8f90b48860ba19f7f
ahananath206-code <ahananath206@gmail.com>
2026-09-22 20:49:28 +0530

    Add files via upload

---
+++ array insertion.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void insert(int arr[], int n, int pos)
+{
+    for (int i = n; i > pos; i--)
+    {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[pos] = 30;
+}
+
+int main()
+{
+    int n = 4;
+    int pos = 2;
+    int arr[6] = {10, 20, 40, 50};
+
+    insert(arr, n, pos);
+
+    n++;
+
+    printf("Array after insertion: ");
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
